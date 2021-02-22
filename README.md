92c9be07cb9bb554c0e6ebebf818df8f38fb3cb6
aridiosilva <aridiosilva@aridiosilva.com>
2021-02-22 12:35:08 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Hash Game
+
+### Simple hash game with ReactJS & NodeJS & JavaScript ES6 & HTML5 & CSS3
+
+## Install
+
+```
+    cd ~/your-directory
+    git clone https://github.com/aridiosilva/reacthashgame.git ./
+    npm install
+```
+
+## Execute
+
+To get started developing right away:
+
+start the development server with `npm start`
+
+## Folder Structure of React Hash game
+
+![Folder Tree Structure]()
---
+++ src/components/Square/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './square.css'
+import X from '../../assets/X.png'
+import O from '../../assets/O.png'
+
+const Square = (props) => {
+
+  const { value, onClick, classCss } = props
+
+  return (
+    <button className={`square ${classCss}`} onClick={ onClick }>
+      {value ? (value === 'X' ? <img src={X} alt="Player X"/> : <img src={O} alt="Player O"/> ) : ''}
+    </button>
+  )
+}
+
+export default Square
